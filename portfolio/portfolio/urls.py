293e7f2df9962c0d1404408e530cc237c3c04d30
Alexander Dimov <alex.lemuel@gmail.com>
2023-08-26 15:12:43 +0300

    added handler404 for 404 error

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -11,3 +11,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'portfolio.contacts.views.custom_page_not_found'
